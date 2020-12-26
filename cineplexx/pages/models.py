05c52488282ea226183cb0a73058411a69646126
aleksandar234 <aleksandar.stojanovic19982@gmail.com>
2020-12-27 00:14:09 +0100

    Zavrsna verzija

--- cineplexx/pages/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Snippet(models.Model):
-    question = models.CharField(max_length=100)
-    choice1 = models.CharField(max_length=100)
-    choice2 = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.question
